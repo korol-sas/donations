6beb88186192afecd71a907e177089ed85ce6197
Korol Oleksandr <korol.sas@gmail.com>
2019-10-22 12:01:12 +0300

    user donations table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@
 class HomeController < AuthenticatedController
   def index
     @donations = current_user.donations
+                             .preload(:donation_type)
+                             .order(donated_at: :desc)
   end
 end
